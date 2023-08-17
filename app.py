9baf01bc608dd09ff4c8cd6e9f3b5779396f4206
PennyDong <sing067808@gmail.com>
2023-08-17 15:08:15 +0800

    First Deploy

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__) #__name__ 代表目前執行的模組
 def home():
     return "Hello Flask"
 
+@app.route("/test")
+def test():
+    return "Test Flask"
+
 if __name__=="__main__": #如果以主程式執行
     app.run() #啟動伺服器
