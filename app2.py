69960a741614b761a83fb66f8dac9e0d23976dd7
PennyDong <sing067808@gmail.com>
2023-08-17 21:37:16 +0800

    靜態檔案設定

--- app2.py
+++ app2.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
-# 建立Application 物件
-app=Flask(__name__) 
+# 建立Application 物件,可以設定靜態檔案的路徑
+app=Flask(
+    __name__,
+    #"靜態檔案的資料夾名稱"
+    static_folder="static", 
+    #"靜態檔案的網址路徑" 
+    static_url_path="/"  
+) 
+
+#所有在static資料夾底下的檔案，都對應到網址路徑/更改的資料夾名稱(也可以改成"/")/檔案名稱
 
 #建立路徑 / 對應的處理函式
 
